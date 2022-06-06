0926dae41c2c3c0dff44eb7ddcc9f88737694479
luthiwd <annattar@gmail.com>
2022-06-06 11:57:07 +0200

    Prueba

--- src/components/NavbarTop.jsx
+++ src/components/NavbarTop.jsx
@@ -38,7 +38,7 @@ function NavbarTop() {
           <Nav className="me-auto">
             <Nav.Link href="/alimentos">De la Huerta</Nav.Link>
             <Nav.Link href="/cajas/create"> Tu EcoCaja </Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown title="Menu" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
