eb041334387920435b20aa83ceac78e50786011e
luthiwd <annattar@gmail.com>
2022-06-09 15:33:52 +0200

    Intentos en borrado de elementos en lascajas de Farmer

--- src/services/foods.services.js
+++ src/services/foods.services.js
@@ -1,21 +1,21 @@
 import service from "./config.services"
-
+//Mostramos todas las Foods de la DB
 const getAllFoodsService = () => {
   return service.get('/alimentos')
 }
-
+//Añadimos un nuevo Food a la DB
 const addNewFoodService = (newFood) => {
   return service.post('/alimentos',newFood)
 }
-
+//Mostramos los detalles de un food en concreto
 const getFoodDetailsService = (id) => {
   return service.get(`/alimentos/${id}`)
 }
-
+//Eliminamos un food de la DB
 const deleteFoodService = (id) => {
   return service.delete(`/alimentos/${id}`)
 }
-
+//Editamos una Food (Sólo puede el Admin)
 const editFoodService = (id, food) => {
   return service.patch(`/alimentos/${id}`, food)
 }
--- src/services/profile.services.js
+++ src/services/profile.services.js
@@ -1,5 +1,5 @@
 import service from './config.services'
-
+//Servicio para subir foto de perfil a Cloudinary
 const uploadService = (uploadForm) => {
   return service.post('/uploader', uploadForm)
 }
